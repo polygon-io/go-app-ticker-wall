4451b6c9bf19382ffcfcb0b0e52e41efd8fca91d
Quinton Pike <qrpike@gmail.com>
2021-11-15 12:35:08 -0500

    Some error handling for not finding screen.

--- leader/screen-update.go
+++ leader/screen-update.go
@@ -2,6 +2,7 @@ package leader
 
 import (
 	"context"
+	"errors"
 
 	"github.com/polygon-io/go-app-ticker-wall/models"
 	"github.com/sirupsen/logrus"
@@ -12,14 +13,22 @@ import (
 func (t *Leader) UpdateScreen(ctx context.Context, newScreenSettings *models.Screen) (*models.Screen, error) {
 	logrus.Debug("Update presentation settings..", newScreenSettings)
 
+	didFind := false
 	t.Lock()
 	for _, client := range t.Clients {
 		// Find the screen we want to update
 		if client.Screen.UUID == newScreenSettings.UUID {
 			client.Screen = newScreenSettings
+			didFind = true
+			break
 		}
 	}
 	t.Unlock()
+
+	// Couldn't find correct screen to update.
+	if !didFind {
+		return nil, errors.New("unable to find screen to update with given UUID")
+	}
 
 	// Update the cluster
 	t.Updates <- &models.Update{
